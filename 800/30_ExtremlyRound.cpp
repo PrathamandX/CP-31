7899325ccf888caa586ad03613c20e261c9d1cf5
Prathamesh Sandip Vispute <24je0674@iitism.ac.in>
2025-11-03 11:57:30 +0530

    Update 30_ExtremlyRound.cpp

--- 800/30_ExtremlyRound.cpp
+++ 800/30_ExtremlyRound.cpp
@@ -1,27 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isRound(int n) {
-    int count=0;
-    while(n>0) {
-        if (n%10)count++;
-        n=n/10;
-        if (count>2)break;
-    }
-    if (count>1)return false;
-    else return true;
-}
-
 int main() {
     int t;
     cin>>t;
     while (t--) {
         int n;
         cin>>n;
-        int count=0;
-        for (int i=1;i<=n;i++) {
-            if (isRound(i)) count++;
+        int index=0;
+        int digit=0;
+        while (n>0) {
+            digit++;
+            index=n%10;
+            n=n/10;
         }
+        int count=((digit-1)*9)+index;
         cout<<count<<endl;
     }
 
